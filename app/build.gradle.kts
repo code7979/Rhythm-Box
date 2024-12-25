edcc40be9082ae60b50748bfc7e5594dae91e0cc
code7979 <abhiketkumar8888@gmail.com>
2024-12-25 06:33:50 +0000

    First Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+}
+
+android {
+  namespace = "com.rhythmbox"
+  compileSdk = 34
+  
+  defaultConfig {
+    applicationId = "com.rhythmbox"
+    minSdk = 26
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
+    
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
+}
+
+dependencies {
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.material)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+}
